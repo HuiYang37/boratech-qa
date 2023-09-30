0ab76388627afa7fda89f94dfcd162dcec201f39
Hui Yang <hui.yang.3737@gmail.com>
2023-09-30 09:59:50 -0400

    updated test suites

--- boratech-qa-cucumber/src/test/java/hui_automation/test_suites/RunParallelSuite.java
+++ boratech-qa-cucumber/src/test/java/hui_automation/test_suites/RunParallelSuite.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("hui_features/boratech")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "hui_automation.steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/html_reports/index.html, json:target/json_reports/index.json")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/html_reports/cucumber.html, json:target/json_reports/cucumber.json")
 @ConfigurationParameter(key = PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(key = PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME, value = "fixed")
 @ConfigurationParameter(key = PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME, value = "1")
