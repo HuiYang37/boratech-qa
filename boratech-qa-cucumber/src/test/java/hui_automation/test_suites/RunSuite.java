04d5300bdb5c99c6bb33faca4069c5e943722124
Hui Yang <hui.yang.3737@gmail.com>
2023-09-30 05:44:17 -0400

    test new case

--- boratech-qa-cucumber/src/test/java/hui_automation/test_suites/RunSuite.java
+++ boratech-qa-cucumber/src/test/java/hui_automation/test_suites/RunSuite.java
@@ -10,8 +10,9 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("hui_features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/html_reports/index.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/html_reports/cucumber.html, json:target/json_reports/cucumber.json")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "hui_automation.steps")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@ui or @e2e")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@login")
 public class RunSuite {
 }
